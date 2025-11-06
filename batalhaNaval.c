257af5fb07de7002ba4e84f308da951a9c3d8e2f
RodrigoScandolara <rodrigo.scandolara86@gmail.com>
2025-11-06 22:25:19 +0000

    Nivel Novato Finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,77 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int index1;
+    int index2; 
+    //Declaração do vetor de tipo char para o a coluna j do jogo
+    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //Declaração da Matriz contendo o tabuleiro do jogo
+    int tabuleiro[10][10] = {
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    };
+
+    printf("\n## JOGO DE BATALHA NAVAL! ##\n");
+    printf("\n");
+    printf("   "); //3 espaços para alinhar a linha com os títulos das colunas com cada coluna
+
+    //Loop para exibir os títulos das colunas
+    for (int colunas = 0; colunas < 10; colunas++){
+        printf("%c ", letras[colunas]);
+
+    }
+
+    printf("\n");
+
+    //Declaração dos vetores dos navios
+    int navio1[] = {3, 3, 3};
+    int navio2[] = {3, 3, 3, 3, 3};
+
+
+    index1 = 2; //Index para fixar a linha do navio 1
+    int j = 4; //Variável "coluna" para navio 1
+    int x = 0; //Posição do vetor do navio 1
+    index2 = 8; //Index para fixar a coluna do navio 2
+    int i = 3; //Variável "linha" para navio 2
+    int z = 0; //Posição do vetor do navio 2
+
+    //Loop para posicionar os navios 1 e 2
+    for (int x = 0; x < 3; x++){
+        tabuleiro[index1][j + x] = navio1[x];
+    }
+
+    for (int z = 0; z < 5 ; z++){
+        tabuleiro[i + z][index2] = navio2[z];
+    }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    //Loop para exibir a numeração das linhas no tabuleiro
+    for (int i = 0; i < 10; i++){
+        printf("%2d", i + 1); // "%2d" para que os números seja exibidos com duas casas, fazendo com que a linha 10 fique alinhada
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        //Loop interno para a exibição do tabuleiro contendo somente água.
+        for (int j = 0; j < 10; j++){
+            printf(" %d", tabuleiro[i][j]);
 
-    return 0;
+            
+        }
+    
+        printf("\n"); //Para que se pule linhas e não se imprima tudo em seguida
+    }
+
+    printf("\n");
+
+return 0;
+
 }
